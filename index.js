81304f662f308aead266e5499b7746534298a549
satyasinha94 <satyasinha94@gmail.com>
2019-01-06 23:17:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching (drivers, string) {
+  return drivers.filter(function (name) {return name.toLowerCase() === string.toLowerCase() });
+}
+
+function fuzzyMatch (drivers, string) {
+  return drivers.filter(function (name) {return name[0] === string[0]});
+}
+
+function matchName (drivers, string) {
+  return drivers.filter(function (driver) {return driver.name === string})
+}
