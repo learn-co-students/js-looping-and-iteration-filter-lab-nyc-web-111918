5699ab49e39b3fcec373fb596637ecbd2742d1f2
josh-sea <joshua.cocciardi@gmail.com>
2019-01-07 09:05:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+function findMatching(drivers, string){
+  const select = drivers.filter(function (driver) {return string.toLowerCase() === driver.toLowerCase();});
+  return select;
+}
+
+function fuzzyMatch(drivers, string) {
+  const select = drivers.filter(function (driver) {return driver.indexOf(string) === 0})
+  return select;
+  }
+
+function matchName(drivers, string) {
+  const solution = [];
+  for (const driver of drivers) {
+    if (driver["name"] === string){
+      solution.push(driver)
+    }
+  }
+  return solution;
+}
