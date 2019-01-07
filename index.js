a1e82d7a277668d02aa154adc7c10bbeae6f56d1
lumrachele <rlum894@gmail.com>
2019-01-07 14:12:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching(list, string){
+  return list.filter(function(name){
+    return name.toUpperCase() === string.toUpperCase();
+  })
+}
+
+
+function fuzzyMatch(list, string){
+  return list.filter(function(name){
+    return name.startsWith(string);
+  })
+}
+
+function matchName(list, string){
+  return list.filter(function(name){
+    return name.name === string
+  })
+}
